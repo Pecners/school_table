566325ce04c4ab663d60fdce7b59fbe8d0aa6499
Spencer Schien <spencer.schien@gmail.com>
2025-05-08 21:06:01 -0500

    added export for big lake data

--- R/write_table_for_export.R
+++ R/write_table_for_export.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(wisconsink12)
+
 clean |> 
   select("School Name" = school_name,
          "Sector" = accurate_agency_type,
@@ -14,4 +17,31 @@ clean |>
          "Limited English Proficiency" = per_lep,
          "Black" = per_b_aa,
          "Latino" = per_hisp_lat) |> 
-  write_csv("data/Milwaukee Schools Data Table 2023.csv")
+  write_csv("data/Milwaukee Schools Data Table 2024.csv")
+
+
+# for Big Lake Data export
+d <- read_rds("data/milwaukee_schools_2024.rda")
+
+d |> 
+  left_join(geocodes) |> 
+  select(school_year,
+         dpi_true_id,
+         lat,
+         long,
+         school_name,
+         accurate_agency_type,
+         grade_band,
+         school_enrollment,
+         ELA,
+         Mathematics,
+         overall_rating,
+         overall_score,
+         sch_ach,
+         sch_growth,
+         per_ed,
+         per_swd,
+         per_lep,
+         per_b_aa,
+         per_hisp_lat) |> 
+  write_csv("data/milwaukee_schools_data_2023-24.csv")
